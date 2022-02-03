94cbf66e470a9ce47b97594b86ead6444d8cb7ae
codkillah <tebedomeny@gmail.com>
2022-02-03 17:34:27 +0200

    add-search-functional

--- client/src/components/TableComponent.js
+++ client/src/components/TableComponent.js
@@ -1,11 +1,13 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { addTickerAction } from '../store/actions/action';
 import { hiddenTickersSelector } from '../selectors/selectors';
 import { useDispatch } from 'react-redux';
 import { BsFillEyeSlashFill } from 'react-icons/bs';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 function TableComponent({ data }) {
+  const [value, setValue] = useState('');
   const hiddenTickers = useSelector(hiddenTickersSelector);
   const dispatch = useDispatch();
 
@@ -16,10 +18,23 @@ function TableComponent({ data }) {
     () => data.filter(({ ticker }) => !hiddenTickers.includes(ticker)),
     [data, hiddenTickers]
   );
+  const filterStocks = stocksData.filter((stocks) => {
+    return stocks.ticker.toLowerCase().includes(value.toLocaleLowerCase());
+  });
 
   return (
     <table className="table table-dark table-striped m-0">
       <thead>
+        <tr>
+          <th colSpan={'1'}>
+            <AiOutlineSearch className="m-2" />
+            <input
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="search"
+            ></input>
+          </th>
+          <th colSpan={'5'}></th>
+        </tr>
         <tr>
           <th scope="col">#</th>
           <th scope="col">Name</th>
@@ -30,7 +45,7 @@ function TableComponent({ data }) {
         </tr>
       </thead>
       <tbody className="border-top-0">
-        {stocksData.map((obj, indx) => {
+        {filterStocks.map((obj, indx) => {
           const isProfit = +obj.price - +obj.change > 0;
           return (
             <tr key={indx + 1}>
